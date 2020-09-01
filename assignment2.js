831374caf15e8bf9a952da6c35b018c11255a21f
Chetanshimpi <70476804+Chetanshimpi@users.noreply.github.com>
2020-09-01 14:57:28 +0530

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,8 @@
+//program for convert minutes to second
+
+function ConMinToSec(minutes)
+{
+    return Math.floor(minutes * 60);
+}
+var MinToSec=ConMinToSec(2); //conver min to sec in js
+document.write("result of con min to sec :- " + MinToSec);
